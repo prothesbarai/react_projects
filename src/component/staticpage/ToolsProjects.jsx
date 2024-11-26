7f3ce152ca6cab49e862232d3f871febaae48bcd
prothesbarai <aspprothesbarai1619@gmail.com>
2024-11-26 10:57:36 +0600

    upto date

--- src/component/staticpage/ToolsProjects.jsx
+++ src/component/staticpage/ToolsProjects.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+
 export default function ToolsProjects() {
+
+    const [value,setValue] = useState(0)
+    const increase = () =>{
+        let num = value+10
+        setValue(num)
+    }
+
+    const decrease = () =>{
+        if(value>0){
+            let num2 = value -10;
+            setValue(num2)
+        }
+    }
+
     return (
         <div>
-            <main className="vh-100">
-                <h1>Tools Projects</h1>
+            <main className="vh-100 align-items-center text-center align-content-center">
+                <div>
+                    <p className="fw-bold fs-1">{value}</p>
+                </div>
+                <button className="btn btn-outline-primary" onClick={increase}>Increase</button>
+                <button className="btn btn-outline-primary mx-5">Tools Projects</button>
+                <button className="btn btn-outline-primary" onClick={decrease}>Decrease</button>
             </main>
         </div>
     )
